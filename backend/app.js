f088cb4a7334e86f6c5ddd337d959f010e5717c1
JonTabiendo <jontabiendo@gmail.com>
2025-01-20 13:21:06 -0500

    middleware updated

--- backend/app.js
+++ backend/app.js
@@ -25,32 +25,34 @@ app.enable('trust proxy')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
+app.use(cors({origin: ["https://clone-tft-helper.onrender.com/"]}));
+
 app.use(helmet.crossOriginResourcePolicy({
   policy: 'cross-origin'
 }));
-// app.use(
-//   csurf({
-//     cookie: {
-//       secure: isProduction,
-//       sameSite: isProduction && "Lax",
-//       httpOnly: true
-//     }
-//   })
-// );
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(function (req, res, next) {
-//   if (process.env.NODE_ENV !== 'development' && !req.secure) {
-//     return res.redirect('https://' + req.headers.host + req.url)
-//   }
+app.use(function (req, res, next) {
+  if (process.env.NODE_ENV !== 'development' && !req.secure) {
+    return res.redirect('https://' + req.headers.host + req.url)
+  }
 
-//   next();
-// })
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
