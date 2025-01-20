f838fd70adfa31be764d6bd6859c07de74afa6cf
JonTabiendo <jontabiendo@gmail.com>
2025-01-20 16:35:13 -0500

    psql require updated

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,6 +1,6 @@
-const { sequelize } = require('./db/models');
+const { db } = require('./db/models');
 
-sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+db.sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
   }
