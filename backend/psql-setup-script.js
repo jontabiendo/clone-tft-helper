627c4f077eb6dcb0518ae6e79c3e4bc9daf609e1
JonTabiendo <jontabiendo@gmail.com>
2025-01-21 14:22:42 -0500

    db.sequelize fixed in psql commands script

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -2,6 +2,6 @@ const db  = require('./db/models');
 
 db.sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
-    await sequelize.createSchema(process.env.SCHEMA);
+    await db.sequelize.createSchema(process.env.SCHEMA);
   }
 });
